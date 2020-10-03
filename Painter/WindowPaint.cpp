8aabc24f4fe63815e4c32bb351b1cdaeb6e0bf70
artem0189 <56150204+artem0189@users.noreply.github.com>
2020-10-03 17:11:57 +0300

    Complete WindowPaint

--- Painter/WindowPaint.cpp
+++ Painter/WindowPaint.cpp
@@ -12,10 +12,17 @@ WindowPaint::~WindowPaint()
 
 VOID WindowPaint::SaveWindow(HWND hWnd, RECT rect)
 {
-
+	image->Create(rect.right - rect.left, rect.bottom - rect.top);
+	HDC imageDc = image->GetDC();
+	PrintWindow(hWnd, imageDc, PW_CLIENTONLY);
+	image->ReleaseDC();
 }
 
 VOID WindowPaint::UpdateWindow(HWND hWnd, RECT rect)
 {
-
+	HDC hDc = GetDC(hWnd);
+	HDC imageDc = image->GetDC();
+	BitBlt(hDc, rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top, imageDc, rect.left, rect.top, SRCCOPY);
+	image->ReleaseDC();
+	ReleaseDC(hWnd, hDc);
 }
